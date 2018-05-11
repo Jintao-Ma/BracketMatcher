48e7a606d65d1a12873ab20f4a6f493d6c9c0c9e
Jintao <crazyclerk000@gmail.com>
2018-05-10 22:57:23 -0500

    another failed test

--- BracketMatcher.Tests/BracketMatcherTests.cs
+++ BracketMatcher.Tests/BracketMatcherTests.cs
@@ -20,7 +20,7 @@ namespace BracketMatcher.Tests
             Assert.Equal(excepted, actual);
         }
 
-        [Theory] 
+        [Theory]
         [InlineData("{}")]
         [InlineData("[]")]
         [InlineData("()")]
@@ -28,7 +28,25 @@ namespace BracketMatcher.Tests
         [InlineData("()wqwee")]
         [InlineData("wqwee()")]
         [InlineData("wwee()213")]
+        [InlineData("w(wee)213")]
+        [InlineData("w(wee)213{}")]
+        [InlineData("w(wee)2[]13{}")]
+        [InlineData("{}w(w[]ee)2[]13{}")]
         public void WhenBracketMatch_ReturnZero(string str)
+        {
+            //Given
+            var excepted = 0;
+
+            //When
+            var actual = sub.Match(str);
+
+            //Then
+            Assert.Equal(excepted, actual);
+        }
+
+        [Theory]
+        [InlineData("(qweqwe")]
+        public void WhenThereIsOneMissMatch_ReturnIndexOfTheMissMatchBracket(string str)
         {
             //Given
             var excepted = 0;
