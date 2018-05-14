aeae32629d6ed1e9421e7460adaf6c832a59150c
Jintao <crazyclerk000@gmail.com>
2018-05-13 21:04:41 -0500

    tests refactoring

--- BracketMatcher.Tests/BracketMatcherTests.cs
+++ BracketMatcher.Tests/BracketMatcherTests.cs
@@ -24,6 +24,20 @@ namespace BracketMatcher.Tests
         [InlineData("{}")]
         [InlineData("[]")]
         [InlineData("()")]
+        public void WhenTheStringIsMatchBrackets_ReturnZero(string str)
+        {
+            //Given
+            var excepted = 0;
+
+            //When
+            var actual = sub.Match(str);
+
+            //Then
+            Assert.Equal(excepted, actual);
+        }
+
+        
+        [Theory]
         [InlineData("(qwe)")]
         [InlineData("()wqwee")]
         [InlineData("wqwee()")]
@@ -32,7 +46,7 @@ namespace BracketMatcher.Tests
         [InlineData("w(wee)213{}")]
         [InlineData("w(wee)2[]13{}")]
         [InlineData("{}w(w[]ee)2[]13{}")]
-        public void WhenBracketMatch_ReturnZero(string str)
+        public void WhenBracketsMatch_ReturnZere(string str)
         {
             //Given
             var excepted = 0;
@@ -47,17 +61,13 @@ namespace BracketMatcher.Tests
         [Theory]
         [InlineData("(qweqwe", 0)]
         [InlineData("qw(eqwe", 2)]
-        [InlineData("qweqw)e", 5)]
-        [InlineData("qweqwe)", 6)]
         [InlineData("{qweqwe", 0)]
         [InlineData("qw[eqwe", 2)]
         [InlineData("qweqw}e", 5)]
         [InlineData("qweqwe]", 6)]
-        [InlineData("qw(e)qwe]", 8)]
-        [InlineData("{}qw(e)qwe]", 10)]
         [InlineData("{qw(e)qwe[]", 0)]
         [InlineData("wq{q[w(e)q]we[]", 2)]
-        public void WhenThereIsOneMissMatch_ReturnIndexOfTheMissMatchBracket(string str, int excepted)
+        public void WhenThereIsOneMissMatchOpenBracket_ReturnIndexOfTheMissMatchBracket(string str, int excepted)
         {
             //When
             var actual = sub.Match(str);
@@ -66,14 +76,21 @@ namespace BracketMatcher.Tests
             Assert.Equal(excepted, actual);
         }
 
-        // [Theory]
-        // public void WhenThereAreMoreThanOneMissMatch_ReturnAllIndexOfTheMissMatchBrackets(string str, int[] excepted)
-        // {
-        //     //Given
 
-        //     //When
+        [Theory]
+        [InlineData("qweqw)e", 5)]
+        [InlineData("qweqwe)", 6)]
+        [InlineData("qweqw}e", 5)]
+        [InlineData("qweqwe]", 6)]
+        [InlineData("qw(e)qwe]", 8)]
+        [InlineData("{}qw(e)qwe]", 10)]
+        public void WhenThereIsOneMissMatchCloseBracket_ReturnIndexOfTheMissMatchBracket(string str, int excepted)
+        {
+            //When
+            var actual = sub.Match(str);
 
-        //     //Then
-        // }
+            //Then
+            Assert.Equal(excepted, actual);
+        }
     }
 }
